843757253a82abca1224c6b68fc23615c2cc149d
Kim Hyelin <myelin0213@gmail.com>
2023-03-22 12:55:08 +0900

    Image path verification2

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -53,7 +53,7 @@ export default function About() {
                         modules={[Pagination, Navigation]}
                     >
                         <SwiperSlide>
-                            <img className="title-1-pic" src="https://hyelin213.github.io/HYLN-PortfolioSite/images/profile_pic.png" alt="프로필 사진" />
+                            <img className="title-1-pic" src="https://hyelin213.github.io/HYLN-PortfolioSite/images/profile_pic.jpg" alt="프로필 사진" />
                         </SwiperSlide>
                         <SwiperSlide>2</SwiperSlide>
                         <SwiperSlide>3</SwiperSlide>
