30c95a212f7d4ffadc0005d90a0fcc2cc6756091
hyelin <myelin0213@gmail.com>
2023-03-24 16:49:14 +0900

    minor modifications2

--- src/components/footerFixed.jsx
+++ src/components/footerFixed.jsx
@@ -4,7 +4,7 @@ export default function FooterFixed() {
 
     return (
         <>
-            <div id="footer-fixed">
+            <div id="footer-fixed" className="mobile-hide">
                 <a href="https://github.com/hyelin213" className="github-link" target="_blank"></a>
                 <p>
                     본 페이지는 상업적 목적이 아닌<br className="mobile-exposure"/>개인 포트폴리오용으로 만들어진 사이트입니다.<br/>
