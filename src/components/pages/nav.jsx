2108708601cc8784fda0a405acff33cf6514e479
Kim Hyelin <myelin0213@gmail.com>
2023-03-29 10:37:07 +0900

    Web responsive test

--- src/components/pages/nav.jsx
+++ src/components/pages/nav.jsx
@@ -1,20 +1,14 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-import AOS from "aos";
-import "aos/dist/aos.css";
 
 const list = [
     {title: 'HOGWARTS SCHOOL', link: 'HogwartsSchool'},
     {title: 'MELON', link: 'Melon'},
-    {title: 'HOW WEAHTER TODAY?', link: 'HWT'},
+    {title: 'HOW WEATHER TODAY?', link: 'HWT'},
     {title: 'PORTFOLIO SITE', link: 'portfolioSite'},
 ]
 
 export default function Nav() {
-
-    useEffect(() => {
-        AOS.init();
-    })
 
     return (
         <div className="project-list" style={{position: 'absolute'}}>
